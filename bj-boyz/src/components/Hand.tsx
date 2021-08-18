aab47ad20dd94403ed234a673cb54f143875240a
eskilsen <eskil_hognestad@hotmail.com>
2021-08-18 16:40:24 +0200

    kjør i kjøresen

--- bj-boyz/src/components/Hand.tsx
+++ bj-boyz/src/components/Hand.tsx
@@ -1,19 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, Deck, StandardDeck } from "fh-cards";
 import CardCC from './CardCC';
+import "./styles/Hand.css"
+
 
 function Hand() {
+  
+
+  let summerizer = (cardHand: Card[]) =>{
+    let sum = 0;
+    cardHand.forEach(card => {
+      sum += parseInt((card.toString().slice(0,-1)))
+    });
+    return setSum(sum);
+  }
+
+
   let deck: Deck = new StandardDeck();
-  {console.log(deck)}
-  let hand: Card[] = deck.draws(2);
+  
+
+  let [cardHand, setCardHand] = useState(deck.draws(4));
+  let [sum, setSum] = useState(0);
+
+  useEffect(() => {
+    summerizer(cardHand);
+  }, [cardHand])
+
+  useEffect(() => {
+    //setCardHand(hand)
+    console.log("Hand endret seg til " + cardHand)
+  }, [cardHand])
+
+  let drawCard = () =>{
+    let tempHand = [...cardHand]
+    tempHand.push(deck.draw())
+
+    console.log(deck.draw())
+    setCardHand(tempHand);
+  }
+
   return (
-    <div className="hand-container">
-      {hand[0].toString()}
-      {hand.map((item) => {
-        return(
-          <CardCC card={item} />
-        )
-      })}
+    <div className = "main-container">
+      <div className="hand-container">
+        {cardHand.map((item) => {
+          return(
+            <CardCC card={item} />
+          )
+        })}
+        </div>
+        <h4 className="summerize">Sum: {sum}</h4>
+        <button onClick={() => drawCard()}>draw</button>
+      
     </div>
   );
 }
