a9db0297e81eb5f03bddc6ba99d734f779ff904a
eskilsen <eskil_hognestad@hotmail.com>
2021-08-18 14:34:55 +0200

    folders baby

---
+++ bj-boyz/src/components/Background.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Background() {
+  return (
+    <div className="Background">
+      <h1>Blackjack baby!!</h1>
+    </div>
+  );
+}
+
+export default Background;
