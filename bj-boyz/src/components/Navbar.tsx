fe3c8ceb4748b11dd4fda0db9db6e1bc043d4f11
eskilsen <eskil_hognestad@hotmail.com>
2021-08-20 11:46:29 +0200

    added home svg in navbar

--- bj-boyz/src/components/Navbar.tsx
+++ bj-boyz/src/components/Navbar.tsx
@@ -1,12 +1,19 @@
 import { ReactSVG } from 'react-svg';
 import JokerHat from "../assets/joker-hat.svg";
 import GitHubLogo from "../assets/github-logo.svg";
+import Home from "../assets/icons8-home.svg";
 import "../components/styles/Navbar.css";
 
 function Navbar() {
     return(
         <nav className ="navbar">
             <ul className="navbar-nav">
+            <li className ="navbar-item">
+                    <a href="/" className="nav-link">
+                        <ReactSVG src ={Home}/>
+                        <span className="link-text">Home</span>
+                    </a>
+                </li>
                 <li className ="navbar-item">
                     <a href="/bjbybjboys" className="nav-link">
                         <ReactSVG src ={JokerHat}/>
