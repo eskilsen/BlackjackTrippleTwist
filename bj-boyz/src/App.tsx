2c77ba73eeb34457066dd15bc4397e59cc275c35
eskilsen <eskil_hognestad@hotmail.com>
2021-08-24 11:46:16 +0200

    håper ikke dette ødelegger

---
+++ bj-boyz/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from './components/Container';
+import Landingpage from './pages/Landingpage';
+
+function App() {
+  return (
+    <div className="main">
+      <Landingpage/>
+    </div>
+  );
+}
+
+export default App;
